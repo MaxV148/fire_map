2b102071610f2b48549ff5bcf825f27262fe2fcd
MaxV148 <maxvater74@googlemail.com>
2025-03-14 10:05:20 +0100

    update pipline

--- backend/tests/init-test-db.sql
+++ backend/tests/init-test-db.sql
@@ -1,2 +1,21 @@
+-- Create fire_backend database
+CREATE DATABASE fire_backend;
+
+-- Connect to fire_backend database
+\c fire_backend
+
+-- Enable PostGIS extension
 CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS postgis_topology; 
+
+-- Create fire_backend_test database
+CREATE DATABASE fire_backend_test;
+
+-- Connect to fire_backend_test database
+\c fire_backend_test
+
+-- Enable PostGIS extension
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- Grant privileges
+GRANT ALL PRIVILEGES ON DATABASE fire_backend TO root;
+GRANT ALL PRIVILEGES ON DATABASE fire_backend_test TO root; 
