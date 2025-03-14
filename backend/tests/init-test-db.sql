91b2f1788e74715c10383d80cb98845fe9217120
MaxV148 <maxvater74@googlemail.com>
2025-03-14 10:08:18 +0100

    update pipline

--- backend/tests/init-test-db.sql
+++ backend/tests/init-test-db.sql
@@ -1,5 +1,5 @@
 -- Create fire_backend database
-CREATE DATABASE fire_backend;
+CREATE DATABASE fire_backend WITH OWNER = root;
 
 -- Connect to fire_backend database
 \c fire_backend
@@ -8,7 +8,7 @@ CREATE DATABASE fire_backend;
 CREATE EXTENSION IF NOT EXISTS postgis;
 
 -- Create fire_backend_test database
-CREATE DATABASE fire_backend_test;
+CREATE DATABASE fire_backend_test WITH OWNER = root;
 
 -- Connect to fire_backend_test database
 \c fire_backend_test
@@ -17,5 +17,10 @@ CREATE DATABASE fire_backend_test;
 CREATE EXTENSION IF NOT EXISTS postgis;
 
 -- Grant privileges
-GRANT ALL PRIVILEGES ON DATABASE fire_backend TO root;
-GRANT ALL PRIVILEGES ON DATABASE fire_backend_test TO root; 
+ALTER DATABASE fire_backend OWNER TO root;
+ALTER DATABASE fire_backend_test OWNER TO root;
+
+-- Grant schema privileges
+GRANT ALL ON SCHEMA public TO root;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO root;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO root; 
